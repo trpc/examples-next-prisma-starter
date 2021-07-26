54f229c3c67066a04dbdb61fd6fba1f7bcd9e4f3
KATT <alexander@n1s.se>
2021-07-26 22:51:46 +0200

    split up cron jobs

--- src/server/routers/cron.ts
+++ src/server/routers/cron.ts
@@ -7,6 +7,7 @@ import { createRouter } from 'server/trpc';
 import { alogliaReindex } from 'server/utils/algolia';
 import { bulkUpsertJobs } from 'server/crawlers/bulkUpsert';
 import { SOURCES } from 'server/crawlers/sources';
+import { z } from 'zod';
 
 export const cronRouter = createRouter()
   .query('reindex', {
@@ -15,34 +16,30 @@ export const cronRouter = createRouter()
     },
   })
   .query('pull', {
-    async resolve({ ctx }) {
-      const sources = await ctx.prisma.source.findMany();
-      const errors: {
-        source: string;
-        error: unknown;
-      }[] = [];
-      for (const source of sources) {
-        console.log('🏃‍♂️', source.slug);
-        const fn = SOURCES[source.slug];
-        if (!fn) {
-          throw new Error(`No such source "${source.slug}"`);
-        }
-        try {
-          console.time(`${source.slug} crawling`);
-          const items = await fn(source);
-          console.timeEnd(`${source.slug} crawling`);
-
-          console.time(`${source.slug} upsert ${items.length} items`);
-          await bulkUpsertJobs({ source, items });
-          console.timeEnd(`${source.slug} upsert ${items.length} items`);
-        } catch (error) {
-          errors.push({ error, source: source.slug });
-        }
+    input: z.string(),
+    async resolve({ ctx, input }) {
+      const source = await ctx.prisma.source.findUnique({
+        where: { slug: input },
+      });
+      if (!source || !SOURCES[source.slug]) {
+        throw new Error(`No such source "${input}"`);
       }
+      console.log('🏃‍♂️', source.slug);
+      const fn = SOURCES[source.slug];
+      if (!fn) {
+        throw new Error(`No such source "${source.slug}"`);
+      }
+      console.time(`${source.slug} crawling`);
+      const items = await fn(source);
+      console.timeEnd(`${source.slug} crawling`);
+
+      console.time(`${source.slug} upsert ${items.length} items`);
+      await bulkUpsertJobs({ source, items });
+      console.timeEnd(`${source.slug} upsert ${items.length} items`);
       console.time('reindexing');
       const algolia = await alogliaReindex();
       console.timeEnd('reindexing');
 
-      return { errors, algolia };
+      return { algolia, items: items.length };
     },
   });
