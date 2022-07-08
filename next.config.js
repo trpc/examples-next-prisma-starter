0049802d34d9d7bb6c4b90b54a2ce0b99905e0df
Omar López <zomars@me.com>
2022-07-08 11:51:57 -0600

    More than a question rather a PR
    What's the difference of using a `getConfig` function VS typing the object directly? @KATT

--- next.config.js
+++ next.config.js
@@ -3,21 +3,10 @@
 const { env } = require('./src/server/env');
 
 /**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function getConfig(config) {
-  return config;
-}
-
-/**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
-module.exports = getConfig({
+/** @type {import("next").NextConfig} */
+const nextConfig = {
   /**
    * Dynamic configuration available for the browser and server.
    * Note: requires `ssr: true` or a `getInitialProps` in `_app.tsx`
@@ -26,4 +15,6 @@ module.exports = getConfig({
   publicRuntimeConfig: {
     NODE_ENV: env.NODE_ENV,
   },
-});
+}
+
+module.exports = nextConfig;
